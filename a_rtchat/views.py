641baeb3b708b64baae19cd3c47b833b9fddffef
ErJun Wu <defyto13579@gmail.com>
2024-05-08 01:31:00 +0800

    create forms.py, update chat.html, views.py

--- a_rtchat/views.py
+++ a_rtchat/views.py
@@ -1,5 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from .models import *
+from .forms import ChatmessageCreateForm
 
-# Create your views here.
+@login_required
 def chat_view(request):
-    return render(request, "a_rtchat/chat.html")
+    chat_group = get_object_or_404(ChatGroup, group_name = "public-chat")
+    chat_messages = chat_group.chat_messages.all()[:30]
+    form = ChatmessageCreateForm()
+
+    if request.method == "POST":
+        form = ChatmessageCreateForm(request.POST)
+        if form.is_valid():
+            message = form.save(commit=False)
+            message.author = request.user
+            message.group = chat_group
+            message.save()
+            return redirect('home')
+    return render(request, "a_rtchat/chat.html", {'chat_messages': chat_messages, 'form': form})
